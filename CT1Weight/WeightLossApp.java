81c81037fd650d048f30592c2536de112ba6eff0
Dillon Mallee <dillondominicmallee@gmail.com>
2023-08-10 20:01:36 +0200

    mod

--- CT1Weight/WeightLossApp.java
+++ CT1Weight/WeightLossApp.java
@@ -6,7 +6,7 @@ public class WeightLossApp {
 
         Scanner scan = new Scanner(System.in);
 
-        Double rMonth = 0.0 , rTotal = 0.0 , rweightavg = 0.0;
+        Double rMonth = 0.0 , rTotal = 0.0 ;
         int iAbove = 0;
         
         for (int i = 1 ; i <= 6 ; i++) {
@@ -20,7 +20,6 @@ public class WeightLossApp {
                     iAbove++;
                 }
         }
-        rweightavg = rTotal / 6 ;
 
         System.out.println("Your total weight los for 6 months were : " + rTotal + " kg");
         System.out.println("In " + iAbove + " Month(s) you lost more than the avrage weight ");
