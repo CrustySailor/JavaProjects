f7c1eb7fe3fc28310c55510fec5506c4a4038e09
Dillon Mallee <dillondominicmallee@gmail.com>
2023-08-10 19:30:28 +0200

    update4

--- Methods1/Question2_1_1.java
+++ Methods1/Question2_1_1.java
@@ -2,10 +2,19 @@ package Methods1;
 
 public class Question2_1_1 {
     
-    public static void main(String[] args){
-
+    public static String determineAngle(int degrees) {
+        if (degrees == 90) {
+            return "The angle is a right angle";
+        } else {
+            return "The angle is not a right angle";
+        }
     }
-    public static double determineAngel(double dDegrees){
-        
+
+    public static void main(String[] args) {
+        int angle = 90; 
+
+        String result = determineAngle(angle);
+        System.out.println(result);
     }
 }
+
